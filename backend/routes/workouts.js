ec3f18bf3dbb45a87e3ca9136c33c2d5bef140b2
PabasaraJay <pabasaramadhumalj@gmail.com>
2023-04-22 01:28:48 +0530

    initial commit

---
+++ backend/routes/workouts.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
+
+const router = express.Router()
+
+//Get all workouts
+router.get('/',getWorkouts)
+
+//Get a single workout
+router.get('/:id',getWorkout)
+
+//Post a new workout
+router.post('/',createWorkout)
+
+//Delete a workout
+router.delete('/:id',deleteWorkout)
+
+//Update a new workout
+router.patch('/:id',updateWorkout)
+
+
+module.exports= router
